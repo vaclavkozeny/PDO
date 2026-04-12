c00b09fd3b4ac12303b7aefd96091f0ca14e32a5
Václav Kožený <vaclav.kozeny@tul.cz>
2026-04-12 12:12:19 +0200

    deploy update

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vitepress'
 
 export default defineConfig({
-  base:"PDO",
   title: "StressTestApp",
   description: "Dokumentace k bakalářské práci",
   themeConfig: {
